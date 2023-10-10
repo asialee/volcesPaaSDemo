66520d59fc24aedd3270fdd7853ef966ef5c52d7
asialee <lyz900715@163.com>
2023-10-10 13:29:41 +0800

    获取客户端IP

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM golang:1.14-alpine
+FROM golang:1.20.5-alpine
 RUN mkdir /app
 COPY ./web.go /app
 # COPY ./output /app/
+# COPY ./main  /app/
 WORKDIR /app
-RUN go build -o main
+RUN go build -o main web.go
 CMD ["/app/main"]
--- web.go
+++ web.go
@@ -1,12 +1,17 @@
-
 package main
 
 import (
     "fmt"
     "net/http"
-    "strings"
+//    "strings"
     "log"
     "io/ioutil"
+    "net"
+)
+
+const (
+	XForwardedFor = "X-Forwarded-For"
+	XRealIP       = "X-Real-IP"
 )
 
 
@@ -18,17 +23,37 @@ func readFile() (string){
     return string(f)
 }
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()  // 解析参数，默认是不会解析的
-    fmt.Println(r.Form)  // 这些信息是输出到服务器端的打印信息
-    fmt.Println("path", r.URL.Path)
-    fmt.Println("scheme", r.URL.Scheme)
-    fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
+func RemoteIp(req *http.Request) string {
+    remoteAddr := req.RemoteAddr
+    if ip := req.Header.Get(XRealIP); ip != "" {
+        remoteAddr = ip
+    } else if ip = req.Header.Get(XForwardedFor); ip != "" {
+        remoteAddr = ip
+    } else {
+        remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
     }
-    fmt.Fprintf(w, readFile()) // 这个写入到 w 的是输出到客户端的
+
+    if remoteAddr == "::1" {
+        remoteAddr = "127.0.0.1"
+    }
+
+    return remoteAddr
+}
+
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+    // r.ParseForm()  // 解析参数，默认是不会解析的
+    // fmt.Println(r.Form)  // 这些信息是输出到服务器端的打印信息
+    // fmt.Println("path", r.URL.Path)
+    // fmt.Println("scheme", r.URL.Scheme)
+    // fmt.Println(r.Form["url_long"])
+    // for k, v := range r.Form {
+    //     fmt.Println("key:", k)
+    //     fmt.Println("val:", strings.Join(v, ""))
+    // }
+    ip := RemoteIp(r)
+    //fmt.Fprintf(w, ip)
+    fmt.Fprintf(w, "client ip:",ip) // 这个写入到 w 的是输出到客户端的
 }
 
 func main() {
